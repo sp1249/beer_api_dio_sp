0f65f623f3284e952fd992dad0b5fdc708255d60
sp1249 <sp1249@uol.com.br>
2021-09-22 19:49:44 -0400

    After classes TDD

--- src/main/java/one/digitalinnovation/beerstock/controller/BeerController.java
+++ src/main/java/one/digitalinnovation/beerstock/controller/BeerController.java
@@ -52,6 +52,7 @@ public class BeerController implements BeerControllerDocs {
     }
 
     @PatchMapping("/{id}/increment")
+    //
     public BeerDTO increment(@PathVariable Long id, @RequestBody @Valid QuantityDTO quantityDTO) throws BeerNotFoundException, BeerStockExceededException {
         return beerService.increment(id, quantityDTO.getQuantity());
     }
